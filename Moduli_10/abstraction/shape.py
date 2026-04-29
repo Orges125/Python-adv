72514b035da012c44d9685ad05139bc783acc8b8
yllimehmeti123 <yllimmehmeti12@gmail.com>
2026-04-29 20:27:19 +0200

    Moduli 10

---
+++ Moduli_10/abstraction/shape.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self,radius):
+      self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+class Square(Shape):
+    def __init__(self,length):
+      self.length = length
+
+    def area(self):
+        return self.length * self.length
+
+circle = Circle(7)
+print(circle.area())
+
+square = Square(7)
+print(square.area())
+
+
